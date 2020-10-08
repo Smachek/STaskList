3437849742042ca9edb98ce55f0254f28b6c12f1
Sergey Smachek <SergeySmachek@gmail.com>
2020-10-08 14:11:19 +0300

    Add mind map document 'project entities'

---
+++ documentation/entities/project-entities.mm
@@ -0,0 +1,92 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Entities" FOLDED="false" ID="ID_164132802" CREATED="1602086624971" MODIFIED="1602087353305" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.5">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Task" LOCALIZED_STYLE_REF="styles.topic" POSITION="right" ID="ID_340519285" CREATED="1601992384289" MODIFIED="1602107042307" HGAP_QUANTITY="24.499999687075587 pt" VSHIFT_QUANTITY="-10.49999968707563 pt" BORDER_WIDTH_LIKE_EDGE="true" BORDER_COLOR_LIKE_EDGE="true">
+<edge STYLE="sharp_bezier" COLOR="#0000ff"/>
+<font SIZE="14"/>
+<node TEXT="Name" LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_1387385461" CREATED="1601995145716" MODIFIED="1602065935505"/>
+<node TEXT="Priority" LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_634016821" CREATED="1601995187954" MODIFIED="1602065943112"/>
+<node TEXT="Start date" LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_1215796289" CREATED="1601995215921" MODIFIED="1602087204714" HGAP_QUANTITY="17.749999888241295 pt" VSHIFT_QUANTITY="-2.9999999105930355 pt">
+<icon BUILTIN="calendar"/>
+</node>
+<node TEXT="Due date" LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_424279697" CREATED="1601995248629" MODIFIED="1602065971239">
+<icon BUILTIN="calendar"/>
+</node>
+<node TEXT="Descrioption" LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_1741456275" CREATED="1601995264040" MODIFIED="1602065977612"/>
+<node TEXT="Done mark" LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_145083438" CREATED="1602087101219" MODIFIED="1602087117637"/>
+<node TEXT="Folder" LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_45358254" CREATED="1602154333956" MODIFIED="1602154394681">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1434669325" STARTINCLINATION="0;16;" ENDINCLINATION="2;-41;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+<node TEXT="Folder" LOCALIZED_STYLE_REF="styles.topic" POSITION="right" ID="ID_1434669325" CREATED="1601995527463" MODIFIED="1602107073960" HGAP_QUANTITY="21.49999977648259 pt" VSHIFT_QUANTITY="2.249999932944773 pt">
+<edge COLOR="#99ff66"/>
+<font SIZE="14"/>
+<node TEXT="Name" LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_1433047975" CREATED="1601997256361" MODIFIED="1602064754034"/>
+<node TEXT="Description" LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_377390403" CREATED="1601997282366" MODIFIED="1602087148957" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="2.9999999105930355 pt"/>
+<node TEXT="Create date" LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_1334106191" CREATED="1602086850177" MODIFIED="1602086877877"/>
+</node>
+</node>
+</map>
