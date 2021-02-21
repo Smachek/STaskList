3475d0c56be55ac2fb7c032aade3af9e193f97c8
Sergey Smachek <SergeySmachek@gmail.com>
2021-02-21 19:13:19 +0300

    Add redirect from home page to Hello.html

--- stasklist-web-app/src/main/java/com/smachek/stasklist/web_app/HelloController.java
+++ stasklist-web-app/src/main/java/com/smachek/stasklist/web_app/HelloController.java
@@ -1,4 +1,4 @@
-package com.smachek.stasklist;
+package com.smachek.stasklist.web_app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
