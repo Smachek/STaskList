76d7eecd9610d368a2581cd81267cbc04c8e1ab7
Sergey Smachek <SergeySmachek@gmail.com>
2021-02-27 22:22:37 +0300

    First implementation  entity "Folder" in DAO with test.

--- dao/src/main/java/com/smachek/dao/jdbc/FolderDaoJdbc.java
+++ dao/src/main/java/com/smachek/dao/jdbc/FolderDaoJdbc.java
@@ -2,12 +2,38 @@ package com.smachek.dao.jdbc;
 
 import com.smachek.dao.FolderDao;
 import com.smachek.model.Folder;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class FolderDaoJdbc implements FolderDao {
+
+    private static final String SQL_GET_ALL_FOLDERS =
+            "SELECT F.ID_FOLDER, F.NAME_FOLDER, F.DESCRIPTION, F.CREATE_DATE FROM FOLDER AS F ORDER BY F.ID_FOLDER";
+
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public FolderDaoJdbc(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
     @Override
     public List<Folder> findAll() {
-        return null;
+        return namedParameterJdbcTemplate.query(SQL_GET_ALL_FOLDERS, new FolderRowMapper());
+    }
+
+    private class FolderRowMapper implements RowMapper<Folder> {
+        @Override
+        public Folder mapRow(ResultSet resultSet, int i) throws SQLException {
+            Folder folder = new Folder();
+            folder.setFolderId(resultSet.getInt("ID_FOLDER"));
+            folder.setFolderName(resultSet.getString("NAME_FOLDER"));
+            folder.setDescription (resultSet.getString("DESCRIPTION"));
+            folder.setCreateDate(resultSet.getDate("CREATE_DATE"));
+            return folder;
+        }
     }
 }
--- dao/src/test/java/com/smachek/dao/jdbc/FolderDaoJdbcTest.java
+++ dao/src/test/java/com/smachek/dao/jdbc/FolderDaoJdbcTest.java
@@ -1,15 +1,27 @@
 package com.smachek.dao.jdbc;
 
 import com.smachek.dao.FolderDao;
+import com.smachek.model.Folder;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import java.util.List;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-dao.xml"})
 public class FolderDaoJdbcTest {
 
+    @Autowired
     private FolderDao folderDao;
 
     @Test
     public void findAllTest() {
+        List<Folder> folders = folderDao.findAll();
+        Assert.assertNotNull(folders);
+        Assert.assertTrue(folders.size() > 0);
     }
 }
