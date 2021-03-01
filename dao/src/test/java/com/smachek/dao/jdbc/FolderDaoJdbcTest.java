cf1c489f189de88a3b898747324c71ccd496112a
Sergey Smachek <SergeySmachek@gmail.com>
2021-03-02 00:48:30 +0300

    Add DAO Folder findById and Create.

--- dao/src/test/java/com/smachek/dao/jdbc/FolderDaoJdbcTest.java
+++ dao/src/test/java/com/smachek/dao/jdbc/FolderDaoJdbcTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -24,4 +25,34 @@ public class FolderDaoJdbcTest {
         Assert.assertNotNull(folders);
         Assert.assertTrue(folders.size() > 0);
     }
+
+    @Test
+    public void findByIdTest(){
+        List<Folder> folders = folderDao.findAll();
+        Assert.assertNotNull(folders);
+        Assert.assertTrue(folders.size() > 0);
+
+        Integer idFolder = folders.get(0).getIdFolder();
+        Folder actualFolder = folderDao.findById(idFolder).get();
+        Assert.assertEquals(folders.get(0), actualFolder);
+    }
+
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void findByIdExceptionalTest(){
+        folderDao.findById(999).get();
+    }
+
+    @Test
+    public void createFolderTest() {
+        List<Folder> folders = folderDao.findAll();
+        Assert.assertNotNull(folders);
+        Assert.assertTrue(folders.size() > 0);
+
+        Folder folder = new Folder("Test Folder");
+        folderDao.create(folder);
+
+        List<Folder> actualFolders = folderDao.findAll();
+        Assert.assertEquals(folders.size()+1, actualFolders.size());
+    }
+
 }
