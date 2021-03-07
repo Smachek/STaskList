c5883e27f8fa74250920dd766d1d8b99af632e59
Sergey Smachek <SergeySmachek@gmail.com>
2021-03-08 01:43:07 +0300

    Add DAO Folder update and delete operations.

--- dao/src/main/java/com/smachek/dao/jdbc/FolderDaoJdbc.java
+++ dao/src/main/java/com/smachek/dao/jdbc/FolderDaoJdbc.java
@@ -22,6 +22,13 @@ public class FolderDaoJdbc implements FolderDao {
             "SELECT F.ID_FOLDER, F.NAME_FOLDER, F.DESCRIPTION, F.CREATE_DATE FROM FOLDER AS F WHERE F.ID_FOLDER = :in_ID_FOLDER";
     private static final String SQL_CREATE_FOLDER =
             "INSERT INTO FOLDER (NAME_FOLDER, DESCRIPTION, CREATE_DATE) VALUES (:in_NAME_FOLDER, :in_DESCRIPTION, :in_CREATE_DATE)";
+    private static final String SQL_CHECK_FOLDER_NAME_EXIST =
+            "SELECT COUNT(NAME_FOLDER) FROM FOLDER WHERE LOWER(NAME_FOLDER) = LOWER(:in_NAME_FOLDER)";
+    private static final String SQL_UPDATE_FOLDER =
+            "UPDATE FOLDER SET NAME_FOLDER = :in_NAME_FOLDER, DESCRIPTION = :in_DESCRIPTION " +
+                    "WHERE ID_FOLDER = :in_ID_FOLDER AND LOWER(:in_NAME_FOLDER) NOT IN (SELECT LOWER(NAME_FOLDER) FROM FOLDER)";
+    private static final String SQL_DELETE_FOLDER =
+            "DELETE FROM FOLDER WHERE ID_FOLDER = :in_ID_FOLDER";
 
     NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     RowMapper rowMapper = BeanPropertyRowMapper.newInstance(Folder.class);
@@ -41,24 +48,39 @@ public class FolderDaoJdbc implements FolderDao {
         return Optional.ofNullable((Folder) namedParameterJdbcTemplate.queryForObject(SQL_GET_FOLDER_BY_ID, sqlParameterSource, rowMapper));
     }
 
+    private boolean isFolderNameUnique(Folder folder) {
+        return namedParameterJdbcTemplate.queryForObject(SQL_CHECK_FOLDER_NAME_EXIST,
+                new MapSqlParameterSource("in_NAME_FOLDER", folder.getNameFolder()), Integer.class) == 0;
+    }
+
     @Override
     public Integer create(Folder folder) {
+        if (!isFolderNameUnique(folder)) {
+            throw new IllegalArgumentException("Folder with the same name already exists in DB");
+        }
         KeyHolder keyHolder = new GeneratedKeyHolder();
         SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
                 .addValue("in_NAME_FOLDER",folder.getNameFolder())
                 .addValue("in_DESCRIPTION", folder.getDescription())
                 .addValue("in_CREATE_DATE", folder.getCreateDate());
         namedParameterJdbcTemplate.update(SQL_CREATE_FOLDER, sqlParameterSource, keyHolder);
-        return Objects.requireNonNull(keyHolder.getKey()).intValue();
+        Integer idFolder = Objects.requireNonNull(keyHolder.getKey()).intValue();
+        folder.setIdFolder(idFolder);
+        return idFolder;
     }
 
     @Override
     public Integer update(Folder folder) {
-        return null;
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+                .addValue("in_ID_FOLDER", folder.getIdFolder())
+                .addValue("in_NAME_FOLDER",folder.getNameFolder())
+                .addValue("in_DESCRIPTION", folder.getDescription());
+        return namedParameterJdbcTemplate.update(SQL_UPDATE_FOLDER, sqlParameterSource);
     }
 
     @Override
     public Integer delete(Integer idFolder) {
-        return null;
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource("in_ID_FOLDER", idFolder);
+        return namedParameterJdbcTemplate.update (SQL_DELETE_FOLDER, sqlParameterSource);
     }
 }
--- dao/src/test/java/com/smachek/dao/jdbc/FolderDaoJdbcTest.java
+++ dao/src/test/java/com/smachek/dao/jdbc/FolderDaoJdbcTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-dao.xml"})
@@ -48,11 +49,68 @@ public class FolderDaoJdbcTest {
         Assert.assertNotNull(folders);
         Assert.assertTrue(folders.size() > 0);
 
-        Folder folder = new Folder("Test Folder");
-        folderDao.create(folder);
+        folderDao.create(new Folder("FOLDER"));
 
         List<Folder> actualFolders = folderDao.findAll();
         Assert.assertEquals(folders.size()+1, actualFolders.size());
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void createFolderWithSameNameTest() {
+        List<Folder> folders = folderDao.findAll();
+        Assert.assertNotNull(folders);
+        Assert.assertTrue(folders.size() > 0);
+
+        folderDao.create(new Folder("FOLDER_SAME"));
+        folderDao.create(new Folder("FOLDER_SAME"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void createFolderWithSameNameDiffCaseTest() {
+        List<Folder> folders = folderDao.findAll();
+        Assert.assertNotNull(folders);
+        Assert.assertTrue(folders.size() > 0);
+
+        folderDao.create(new Folder("FOLDER_SAME"));
+        folderDao.create(new Folder("FOLDER_Same"));
+    }
+
+    @Test
+    public void updateFolderTest() {
+        List<Folder> folders = folderDao.findAll();
+        Assert.assertNotNull(folders);
+        Assert.assertTrue(folders.size() > 0);
+
+        Folder folder = folders.get(0);
+        folder.setNameFolder("FOLDER_update");
+        folder.setDescription("FOLDER_DESCRIPTION_update");
+        folderDao.update(folder);
+
+        Optional<Folder> actualFolder = folderDao.findById(folder.getIdFolder());
+        Assert.assertEquals("FOLDER_update", actualFolder.get().getNameFolder());
+        Assert.assertEquals("FOLDER_DESCRIPTION_update", actualFolder.get().getDescription());
+    }
+
+    @Test
+    public void updateFolderWithSameNameTest() {
+        List<Folder> folders = folderDao.findAll();
+        Assert.assertNotNull(folders);
+        Assert.assertTrue(folders.size() > 0);
+
+        Folder folder = folders.get(0);
+        folder.setNameFolder(folders.get(1).getNameFolder());
+        folderDao.update(folder);
+    }
+
+    @Test
+    public void deleteFolderTest() {
+        Integer deleteIdFolder = folderDao.create(new Folder("FOLDER_DELETE"));
+        List<Folder> folders = folderDao.findAll();
+
+        int deletedCount = folderDao.delete(deleteIdFolder);
+
+        List<Folder> actualFolders = folderDao.findAll();
+        Assert.assertEquals(1, deletedCount);
+        Assert.assertEquals(folders.size()-1, actualFolders.size());
+    }
 }
